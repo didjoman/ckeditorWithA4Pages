19504139efe5e3a5fc141a96c3aa2b8c62c46f38
supralog-arup <arupp@supralog.com>
2016-04-05 13:29:10 +0200

    Correct bad linkings

--- ckeditor/config.js
+++ ckeditor/config.js
@@ -1,10 +1,28 @@
-/**
- * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
- */
 
 CKEDITOR.editorConfig = function( config ) {
-	// Define changes to default configuration here. For example:
-	// config.language = 'fr';
-	// config.uiColor = '#AADC6E';
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		'/',
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'paragraph', groups: [ 'blocks', 'align', 'list', 'indent', 'bidi', 'paragraph' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+	
+	config.removePlugins = 'magicline';
+
+	config.removeButtons = 'Source,NewPage,Templates,ShowBlocks,About,Image,Flash,Table,HorizontalRule,Smiley,SpecialChar,Iframe,PageBreak,Unlink,Anchor,Language,BidiRtl,BidiLtr,CreateDiv,Blockquote,RemoveFormat,Form,Checkbox,Radio,TextField,Select,Textarea,Button,ImageButton,HiddenField,SelectAll';
+	config.allowedContent = true;
+	config.contentsCss = './style.css';
+	config.width = '90%';
+	config.height = '30em'; 
+
 };
